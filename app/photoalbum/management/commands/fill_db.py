29ef09e5d42c945cc758584d9c18cb7e686bc6aa
nanku <fireloki87@gmail.com>
2022-10-07 22:46:49 +0300

    added fill database used management command.

---
+++ app/photoalbum/management/commands/fill_db.py
@@ -0,0 +1,24 @@
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import User
+from mixer.backend.django import mixer
+from django.core.management import BaseCommand
+from photoalbum.models import Album,Photo
+
+class Command(BaseCommand):
+    help = "Fill the database with data"
+
+    def handle(self, *args, **kwargs):
+        # FIXME только здесь создает thumbnails не в той директории. (не в images/thumbnails/, а рядом в thumbnails)
+        User.objects.all().delete()
+        Album.objects.all().delete()
+        Photo.objects.all().delete()
+
+        user = get_user_model().objects.create_superuser(username='admin', password='123')
+        admin_album = mixer.blend(Album, username=user)
+        mixer.cycle(2).blend(Photo, album__username=user, album=admin_album)
+
+        users = mixer.cycle(2).blend(User)
+        for user in users:
+            album = mixer.cycle(3).blend(Album, username=user)
+            mixer.cycle(3).blend(Photo, album=mixer.sequence(*album))
+
