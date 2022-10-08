c2d351a36f32cc139bff33e329c6664ef6d08be4
nanku <fireloki87@gmail.com>
2022-10-08 16:09:55 +0300

    remove signals in apps.py

--- app/photoalbum/apps.py
+++ app/photoalbum/apps.py
@@ -4,6 +4,3 @@ from django.apps import AppConfig
 class PhotoalbumConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'photoalbum'
-
-    def ready(self):
-        from . import signals
